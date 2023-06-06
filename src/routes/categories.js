6fe6e2bfeafafb210143efe310b88010eab7c1b7
hoangbru <hoangbru07@gmail.com>
2023-06-07 04:34:44 +0700

    feat: update routes category, product refs: AH-17

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -3,9 +3,7 @@ import { checkPermission } from '../middlewares/checkPermission';
 import express from 'express';
 
 const router = express.Router();
-router.route("/categories").get(getAll).post(create)
-router.get("/categories/:id", get)
-router.delete("/categories/:id", remove)
-router.patch("/categories/:id", update)
+router.route("/categories").get(getAll).post(checkPermission,create)
+router.route("/categories/:id").get(get).delete(checkPermission, remove).patch(checkPermission, update)
 
 export default router;
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,12 +4,12 @@ import { checkPermission } from '../middlewares/checkPermission';
 import uploadCloud from '../middlewares/cloudinary';
 
 const router = express.Router();
-router.route("/products").get(getAll).post(create)
-router.route("/products/:id").get(get).patch(update).delete(remove)
+router.route("/products").get(getAll).post(checkPermission, uploadCloud.single('thumbnail'), create)
+router.route("/products/:id").get(get).patch(checkPermission,update).delete(checkPermission,remove)
+router.patch("/products/restore/:id", checkPermission, restore)
+router.delete("/products/:id/force", checkPermission, forceDelete)
 router.get("/products/:slug", getSlug)
 router.get("/admin/products/:id", get)
 router.get("/admin/products/trash", getTrash)
-router.patch("/products/restore/:id", restore)
-router.delete("/products/:id/force", forceDelete)
 
 export default router;
