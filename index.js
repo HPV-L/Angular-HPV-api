b0a455960700671a072b41707502adf8f4b9b9d7
takemichi <hoangbru07@gmail.com>
2023-07-19 21:59:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import express from "express";
+import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import morgan from "morgan";
 import productsRouter from "./src/routes/products.js";
 import categoriesRouter from "./src/routes/categories.js";
 import userRouter from "./src/routes/auth.js";
@@ -12,24 +11,25 @@ import colorRouter from "./src/routes/color.js";
 import statusRouter from "./src/routes/status.js";
 import commentRouter from "./src/routes/comment.js";
 
-import { connectOnlDB, connectLocalDB } from "./src/config/connect.js";
+import { connectOnlDB } from "./src/config/connect.js";
 
 const app = express();
 const port = 8080
 dotenv.config();
 
 app.use(express.json());
-app.use(morgan("tiny"));
 app.use(cors());
 
-app.use("/api", productsRouter);
-app.use("/api", categoriesRouter);
-app.use("/api", userRouter);
-app.use("/api", orderRouter);
-app.use("/api", sizeRouter);
-app.use("/api", colorRouter);
-app.use("/api", statusRouter);
-app.use("/api", commentRouter);
+app.use("/api", 
+productsRouter,
+categoriesRouter,
+userRouter,
+orderRouter,
+sizeRouter,
+colorRouter,
+statusRouter,
+commentRouter
+);
 
 mongoose.set('strictQuery', false);
 // mongodb local
